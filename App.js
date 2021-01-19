f7c565bbe036174db1e16fc994320b7db0bdeeec
Erik Andersson <erik.anderzzon@gmail.com>
2021-01-19 20:55:08 +0100

    Update App.js
    Added Flat List

--- App.js
+++ App.js
@@ -1,22 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
+const DATA = [
+  {
+    id: "1",
+    title: "Första övningen"
+  },
+  {
+    id: "2",
+    title: "Stå på tå"
+  },
+  {
+    id: "3",
+    title: "Flaxa med armarna"
+  },
+  {
+    id: "4",
+    title: "Stå på ett ben"
+  },
+]
 
-const ListItem = ({title}) => {
+const ListItem = ({title, id}) => {
   return (
-    <View style = {styles.listItem}>
-      <Text>{title}</Text>
+    <View style = {styles.listItem}    >
+      <Text
+      onPress={() => console.log(id)}
+      >{title}</Text>
     </View>
+  )
+}
+
+const ActivityList = () => {
+const renderItem = ({item}) => (
+  <ListItem title = {item.title}/>
+)
+const handlePress = ({item}) => {
+  console.log("ID: ", item.id)
+}
+
+  return (
+    <FlatList style = {styles.activityList}
+      data = {DATA}
+      renderItem = {({item, index}) =>
+        <ListItem title = {item.title} id = {item.id}
+          onPress= {handlePress}
+          onPress={() => console.log("Hej")}
+        />
+      }
+      
+      
+      />
   )
 }
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ListItem title = {"hej"}/>
+    <SafeAreaView style={styles.container}>
+      <ActivityList/>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -30,9 +73,15 @@ const styles = StyleSheet.create({
   },
 
   listItem: {
-    width: '100%',
+    //width: '100%',
     alignContent: 'flex-start',
     padding: 20,
-    backgroundColor: 'grey'
+    margin: 5,
+    borderColor: 'grey',
+    borderWidth: 2
   },
+
+  activityList: {
+    width: '100%'
+  }
 });
